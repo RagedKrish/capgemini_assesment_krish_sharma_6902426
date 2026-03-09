6fdaf22feee4481cb7e70261c224a99583f27488
RagedKrish <krish18sharma21@gmail.com>
2026-03-09 16:20:56 +0530

    completed task1 day14

--- assesment day 14 (09.03.2026)/tests/day14_task1.spec.ts
+++ assesment day 14 (09.03.2026)/tests/day14_task1.spec.ts
@@ -4,7 +4,7 @@ test("qspider_login",async ({page})=>{
     await page.goto("https://demoapps.qspiders.com/ui?scenario=1");
     await expect(page.locator('//div[@class="scenarios_1 "]')).toBeAttached();
     await expect(page.locator('//div[@class="scenarios_1 "]')).toBeVisible();
-    console.log(await expect(page.locator('//div[@class="scenarios_1 "]')).toContainText("Scenarios"));
+    await expect(page.locator('//div[@class="scenarios_1 "]')).toContainText("Scenarios");
     await page.getByRole('textbox',{name:'name'}).fill('Krish',{timeout:20000});
     await page.getByRole('textbox',{name:'email'}).fill('krish18sharma21@gmail.com');
     await page.getByRole('textbox',{name:'password'}).fill('12345678');
